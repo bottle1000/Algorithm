d99d96d504b30a684dd3c337d67625552a434045
1000E <83916614+bottle1000@users.noreply.github.com>
2024-11-11 11:25:37 +0900

    Update 문자열 다루기 기본.java

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -4,16 +4,16 @@ class Solution {
     public boolean solution(String s) {
         boolean answer = true;
         
-        if (s.length() != 4 && s.length() != 6) {
-           answer = false;
-        }
-        
-         for (int i = 0; i < s.length(); i++) {
+       if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < s.length(); i++) {
                 char ch = s.charAt(i);
                 if(Character.isLetter(ch)) {
                     answer = false;
                 } 
             }
+        } else {
+            answer = false;
+        }
         
         return answer;
     }
