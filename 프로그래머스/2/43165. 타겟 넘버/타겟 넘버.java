580354bd9260ee6329959d004891938bf92fc5e0
1000E <83916614+bottle1000@users.noreply.github.com>
2026-04-03 23:55:43 +0900

    [level 2] Title: 타겟 넘버, Time: 5.87 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+        return answer;
+    }
+    
+    public void dfs(int L, int sum, int[] numbers, int target) {
+        
+        if(L == numbers.length){
+            if(sum == target) {
+                answer ++;
+            }
+            
+        } else {
+            dfs(L+1, sum+numbers[L], numbers, target);
+            dfs(L+1, sum-numbers[L], numbers, target);
+        }
+    }
+}
