978e9c5c6906669f20c09c56470e98e56f479ca4
1000E <83916614+bottle1000@users.noreply.github.com>
2026-04-05 20:20:34 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
+        boolean[] visited = new boolean[n];
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                answer++;
+                dfs(i, n, computers, visited);
+            }
+        }
+        return answer;
+    }
+    
+    public void dfs(int i, int n, int[][] computers, boolean[] visited) {
+        visited[i] = true;
+        
+        for (int j = 0; j < n; j++) {
+            if(j != i && computers[i][j] == 1 && !visited[j]) {
+                dfs(j, n, computers, visited);
+            }
+        }
+    }
+}
